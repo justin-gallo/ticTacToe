4c70ed21f6cb5f5d5c0b4192fe2d5e84f16903cb
Justin Gallo <contact@justingallo.dev>
2022-03-06 19:45:48 -0500

    Creates partial gameplay logic

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,42 +1,98 @@
+// Factory to create new player and associate a "piece" with them
 const Player = (piece) => {
     this.piece = piece;
 
-    const returnPiece = () => {
-        return this.piece;
+    const sayPiece = () => {
+        return(piece);
     }
 
     return {
-        returnPiece
+        sayPiece,
     }
 }
 
 //gameboard module: holds functions to manipulate the array represeting the gameboard
 const gameboard = (() => {
-    let board = ["X","O","X","X","O","X","X","O","X"];
+    let board = ["","","","","","","","",""];
 
+    // enables setting a specific cell to a player's piece
+    const setCellValue = (index, piece) => {
+        if (index < board.length) {
+            board[index] = piece;
+        }
+    }
+    // enables fetching which piece is on a specific cell
+    const getCellValue = (index) => {
+        if (index < board.length) {
+            return board[index];
+        }
+    }
+    // iterates through gameboard array, replaces everything with ""
     const reset = () => {
-        for (let i = 0; i <= board.length; i++) {
+        for (let i = 0; i < board.length; i++) {
             board[i] = "";
         }
     }
 
     return {
-        reset,
+        reset, 
+        setCellValue,
+        getCellValue,
     }
 })();
 
 //displayController module: Everything DOM related (Click events), references gameboard module. 
 const displayController = (() => {
-    const board = document.querySelectorAll(".cell");
-    const message = document.getElementById("message");
+    const boardCells = document.querySelectorAll(".gameCell");
+    const message = document.getElementById("status");
     const reset = document.getElementById("resetBtn");
 
+    for (let i = 0; i < boardCells.length; i++) {
+        boardCells[i].addEventListener("click", function() {
+            gameController.takeTurn(i);
+            updateBoard();
+        })
+    }
 
+    const updateBoard = () => {
+        for (let i = 0; i < boardCells.length; i++) {
+            boardCells[i].textContent = gameboard.getCellValue(i);
+        }
+    }
+
+    return {
+        //functions to return
+    }
 })();
 
 //gameController module: controls gameplay, turns, game logic. 
 const gameController = (() => {
-    player1 = Player("X");
-    player2 = Player("O");
+    const playerX = Player("X");
+    const playerO = Player("O");
+    let turn = 1;
 
+    //Determines who's turn it is (X plays on odd turns, O plays on evens)
+    function getCurrentPlayerPiece() {
+        if (turn % 2 == 1) {
+            return playerX.sayPiece();
+        } else {
+            return playerO.sayPiece();
+        }
+    }
+
+    const takeTurn = (index) => {
+        if (gameboard.getCellValue(index) === "") {
+            gameboard.setCellValue(index, getCurrentPlayerPiece());
+        } else {
+            return;
+        };
+        turn++;
+    };
+
+    return {
+        takeTurn,
+        getCurrentPlayerPiece,
+        playerX, 
+        playerO,
+    }
 })();
